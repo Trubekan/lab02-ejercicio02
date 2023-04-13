356156aa5e957749523b7674445976d7acf8e66b
Enzo <enzo.velarde@tecsup.edu.pe>
2023-04-12 20:20:22 -0500

    cambios al logo

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const Login = () => {
+  const [user, setUsername] = useState("");
+  const [clave, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log(`Usuario: ${user}, Contraseña: ${clave}`);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize:70}}>Inicio de Sesion</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setUsername(text)}
+        value={user}
+        placeholder="Usuario"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setPassword(text)}
+        value={clave}  
+        placeholder="Contraseña"
+        secureTextEntry={true}
+      />
+      <Image
+        source={{ uri: "https://i.ibb.co/JmHG5Hv/Dragon-Ball-anime-logo.png" }}
+        style={styles.logo}
+      />
+      <TouchableOpacity
+        onPress={handleLogin}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Ingresar</Text>
+      </TouchableOpacity>
+      
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +47,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 450,
+    height: 300,
+    marginBottom: 30,
+  },
+  input: {
+    height: 50,
+    width: '80%',
+    marginVertical: 10,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: '#FF6600',
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 24,
+  },
+  
 });
+
+export default Login;
